09824c14e6806e8056e730c2b0bf40f5cb9d2a82
cijin <cijin@seagin.me>
2023-10-23 04:41:46 +0700

    minor space updates

--- ast/ast.go
+++ ast/ast.go
@@ -161,9 +161,9 @@ func (fl *FunctionLiteral) String() string {
 	}
 
 	out.WriteString(fl.TokenLiteral())
-	out.WriteString("( ")
+	out.WriteString("(")
 	out.WriteString(strings.Join(params, ", "))
-	out.WriteString(" )")
+	out.WriteString(")")
 	out.WriteString(fl.Body.String())
 
 	return out.String()
